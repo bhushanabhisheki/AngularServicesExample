548252cac6f0be2f2f23e923011ed590490782a1
Bhushan Abhisheki <bhushanabhisheki@gmail.com>
2021-12-15 08:59:49 +0530

    Retrieving Query Parameters and Fragments

--- AngularGuideApps/projects/router-application/src/app/servers/edit-server/edit-server.component.ts
+++ AngularGuideApps/projects/router-application/src/app/servers/edit-server/edit-server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -12,7 +13,10 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) {}
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.server = this.serversService.getServer(1);
@@ -20,6 +24,14 @@ export class EditServerComponent implements OnInit {
       this.serverName = this.server?.name;
       this.serverStatus = this.server?.status;
     }
+
+    //fetch query parameters and fragment from external
+    console.log(this.route?.snapshot.queryParams);
+    console.log(this.route?.snapshot.fragment);
+
+    //fetch query parameters and fragment from within component
+    this.route.queryParams.subscribe((params) => {});
+    this.route.fragment.subscribe((fragment) => {});
   }
 
   onUpdateServer() {
