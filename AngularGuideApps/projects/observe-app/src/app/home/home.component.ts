a529b8e31959c0652c822191f12fe13146e3a5ac
Bhushan Abhisheki <bhushanabhisheki@gmail.com>
2021-12-28 18:50:01 +0530

    understanding obeservable

--- AngularGuideApps/projects/observe-app/src/app/home/home.component.ts
+++ AngularGuideApps/projects/observe-app/src/app/home/home.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
+export class HomeComponent implements OnInit, OnDestroy {
+  private timerSubscription: Subscription | undefined;
+  constructor() {}
 
   ngOnInit() {
+    this.timerSubscription = interval(1000).subscribe((count: number) => {
+      console.log(count);
+    });
   }
 
+  ngOnDestroy() {
+    if (this.timerSubscription) this.timerSubscription.unsubscribe();
+  }
 }
