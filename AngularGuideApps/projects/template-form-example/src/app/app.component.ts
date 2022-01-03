01f861121dd6b1114ca9b2d6f03980bcc2fea78b
Bhushan Abhisheki <bhushanabhisheki@gmail.com>
2022-01-03 12:17:03 +0530

    TD: Grouping Form Controls

--- AngularGuideApps/projects/template-form-example/src/app/app.component.ts
+++ AngularGuideApps/projects/template-form-example/src/app/app.component.ts
@@ -8,6 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('myform') form: NgForm | undefined;
+  defaultQuestion = 'teacher';
+  answer = '';
 
   suggestUserName() {
     const suggestedName = 'Superuser';
