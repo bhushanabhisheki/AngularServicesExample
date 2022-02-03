e9a615bed85875f0799ac423619bae68a3e955bc
Bhushan Abhisheki <bhushanabhisheki@gmail.com>
2022-02-03 18:18:52 +0530

    Services & Components Working Together

--- AngularGuideApps/projects/http-examples/src/app/app.component.ts
+++ AngularGuideApps/projects/http-examples/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Post } from './post.model';
+import { PostsService } from './posts.service';
 
 @Component({
   selector: 'app-root',
@@ -14,49 +15,32 @@ export class AppComponent implements OnInit {
   baseUrl: string =
     'https://newsample-fab58-default-rtdb.firebaseio.com/posts.json';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private postsService: PostsService) {}
 
   ngOnInit() {
-    this.fetchPosts();
+    this.isFetchingPosts = true;
+    this.postsService.fetchPosts().subscribe((posts) => {
+      this.loadedPosts = posts;
+      this.isFetchingPosts = false;
+    });
   }
 
   onCreatePost(postData: Post) {
     // Send Http request
-    console.log(postData);
-    this.http
-      .post<{ name: string }>(this.baseUrl, postData)
-      .subscribe((responseData) => {
-        console.log(responseData.name);
-        this.fetchPosts();
-      });
-  }
-
-  private fetchPosts() {
-    this.isFetchingPosts = true;
-    this.http
-      .get<{ [key: string]: Post }>(this.baseUrl)
-      .pipe(
-        map((responseData: { [key: string]: Post }) => {
-          const postArray: Post[] = [];
-          for (const key in responseData) {
-            if (responseData.hasOwnProperty(key)) {
-              postArray.push({ ...responseData[key], id: key });
-            }
-          }
-
-          return postArray;
-        })
-      )
-      .subscribe((posts: Post[]) => {
-        console.log(posts);
-        this.loadedPosts = posts;
-        this.isFetchingPosts = false;
+    this.postsService
+      .createAndStorePost(postData.title, postData.content)
+      .subscribe((data) => {
+        this.onFetchPosts();
       });
   }
 
   onFetchPosts() {
+    this.isFetchingPosts = true;
     // Send Http request
-    this.fetchPosts();
+    this.postsService.fetchPosts().subscribe((posts) => {
+      this.loadedPosts = posts;
+      this.isFetchingPosts = false;
+    });
   }
 
   onClearPosts() {
---
+++ AngularGuideApps/projects/http-examples/src/app/posts.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Post } from './post.model';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostsService {
+  baseUrl: string =
+    'https://newsample-fab58-default-rtdb.firebaseio.com/posts.json';
+  constructor(private http: HttpClient) {}
+
+  createAndStorePost(title: string, content: string) {
+    const postData: Post = { title: title, content: content };
+    return this.http.post<{ name: string }>(this.baseUrl, postData);
+  }
+
+  fetchPosts() {
+    return this.http.get<{ [key: string]: Post }>(this.baseUrl).pipe(
+      map((responseData: { [key: string]: Post }) => {
+        const postArray: Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postArray.push({ ...responseData[key], id: key });
+          }
+        }
+
+        return postArray;
+      })
+    );
+  }
+}
